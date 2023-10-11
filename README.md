5bfad552b21bb651b47f0fae29028b0e6aeadd45
Marcello Matos <marcellomatosxads@gmail.com>
2023-10-10 21:24:19 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# trabalho_oficial_html_css_js
+ Trabalho Faculdade
---
+++ portifolio.js
@@ -0,0 +1,28 @@
+const form = document.querySelector("#form")
+const nameInput = document.querySelector("#name")
+const emailInput = document.querySelector("#email")
+const passwordInput = document.querySelector("#password")
+const jobSelect = document.querySelector("#job")
+const messageTextarea = document.querySelector("#message")
+
+form.addEventListener("submit", (Event) => {
+    Event.preventDefault();
+
+    //Verifica se o nome está vazio.//
+    if(nameInput.value === "") {
+        alert('Por favor, preencha seu Nome.');
+        return;
+    }
+//Se todos os campos estiver corretamente preenchidos, envie o formulário.
+form.submit()
+});
+
+//Função que valida e-mail
+function isEmailValid(email) {
+    //Cria uma regex para validar email
+    const emailRegex = new RegExp(
+        //usuario12@host.com.br
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\./
+    )
+}
+
